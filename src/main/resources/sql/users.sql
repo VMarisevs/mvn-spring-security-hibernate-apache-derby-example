3ba6ff5e75342c4773a99202c01c728a9c20daca
Vladislavs Marisevs <vxm93@eis.equifax.com>
2016-08-18 12:22:42 +0100

    using Bcrypt password encryption

--- src/main/resources/sql/users.sql
+++ src/main/resources/sql/users.sql
@@ -4,13 +4,13 @@ CREATE TABLE authorities(
 );
 CREATE TABLE users(
   username varchar(40) NOT NULL PRIMARY KEY,
-  password varchar(40) NOT NULL,
+  password varchar(60) NOT NULL,
   enabled INT NOT NULL
 );
 
 INSERT INTO authorities(username, authority)VALUES('andy', 'ROLE_USER');
 INSERT INTO authorities(username, authority)VALUES('root', 'ROLE_ADMIN');
 
-INSERT INTO users(username, password, enabled)VALUES('andy', '1234', 1);
-INSERT INTO users(username, password, enabled)VALUES('root', 'root', 1);
+INSERT INTO users(username, password, enabled)VALUES('andy', '$2a$10$F.aNLnrqrsGSm0rAJnjY1Ow3FIsj3i76FPtb0Qdzfq8Ap25S0AaOm', 1);
+INSERT INTO users(username, password, enabled)VALUES('root', '$2a$06$yA3KZ/jFeab3U2XD/XECR.NWW2dXJorebAIcWp6eBnOtIerUNjEMe', 1);
 
