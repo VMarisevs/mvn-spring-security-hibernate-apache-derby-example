1f0cc1aa9e760dabb2390a6f98966ac4a982e00d
Vladislavs Marisevs <vxm93@eis.equifax.com>
2016-08-18 13:44:19 +0100

    different access roles

--- src/main/resources/sql/users.sql
+++ src/main/resources/sql/users.sql
@@ -4,13 +4,16 @@ CREATE TABLE authorities(
 );
 CREATE TABLE users(
   username varchar(40) NOT NULL PRIMARY KEY,
+  name varchar(40) NOT NULL,
   password varchar(60) NOT NULL,
   enabled INT NOT NULL
 );
 
 INSERT INTO authorities(username, authority)VALUES('andy', 'ROLE_USER');
 INSERT INTO authorities(username, authority)VALUES('root', 'ROLE_ADMIN');
+INSERT INTO authorities(username, authority)VALUES('rootandy', 'ROLE_USER,ROLE_ADMIN');
 
-INSERT INTO users(username, password, enabled)VALUES('andy', '$2a$10$F.aNLnrqrsGSm0rAJnjY1Ow3FIsj3i76FPtb0Qdzfq8Ap25S0AaOm', 1);
-INSERT INTO users(username, password, enabled)VALUES('root', '$2a$06$yA3KZ/jFeab3U2XD/XECR.NWW2dXJorebAIcWp6eBnOtIerUNjEMe', 1);
+INSERT INTO users(username, name, password, enabled)VALUES('andy', 'Andy', '$2a$10$F.aNLnrqrsGSm0rAJnjY1Ow3FIsj3i76FPtb0Qdzfq8Ap25S0AaOm', 1);
+INSERT INTO users(username, name, password, enabled)VALUES('root', 'Root', '$2a$06$yA3KZ/jFeab3U2XD/XECR.NWW2dXJorebAIcWp6eBnOtIerUNjEMe', 1);
+INSERT INTO users(username, name, password, enabled)VALUES('rootandy', 'Root', '$2a$06$yA3KZ/jFeab3U2XD/XECR.NWW2dXJorebAIcWp6eBnOtIerUNjEMe', 1);
 
--- src/main/webapp/WEB-INF/jsp/hello.jsp
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+
+<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags" %>    
+    
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -8,5 +11,11 @@
 </head>
 <body>
 	<h1>Hello!!!</h1>
+	
+	<p> Welcome <sec:authentication property="name"/>
+	
+	<sec:authorize ifAnyGranted="ROLE_ADMIN">
+		<h1>ADMIN!</h1>
+	</sec:authorize>
 </body>
 </html>
